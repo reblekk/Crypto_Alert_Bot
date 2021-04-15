5a9db8f4c864731bf2d6ac546ba626385da1457a
fahim <69826583+reblekk@users.noreply.github.com>
2021-04-15 19:55:16 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,118 @@
 const { Cryptonator } = require('node-crypto-api');
-
 const cryptonator = new Cryptonator();
-let __price, i;
-const fs = require('fs')
-const replace = require('replace-in-file');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('sheet.json')
+const db = low(adapter)
+var TelegramBot = require('node-telegram-bot-api');
+var token = '1552990411:AAEsiJpLC0ZGnPdSYwGZfd6T01p_M0nzrRM';
+var bot = new TelegramBot(token, { polling: true });
 
-//ticker
 
+
+//btc usd 56000 up
+
+bot.onText(/\/set (.+)/, function(msg, match) {
+
+    var chatId = msg.chat.id;
+
+    const string = match[1];
+    var __world = string.split(" ")
+
+
+    __get_ticker = __world[0];
+    __get_compare = __world[1];
+    __get_com_price = __world[2]
+    __get_upndown = __world[3]
+    db.defaults({ posts: [], count: 0, })
+        .write()
+
+
+    let don = db.get('posts')
+        .size()
+        .value()
+
+    db.get('posts')
+        .push({ id: don, chatId: chatId, ticker: __get_ticker, compare: __get_compare, com_price: __get_com_price, upndown: __get_upndown, status: "active" })
+        .write()
+
+});
+
+
+
+//for looping and checking price
 setInterval(() => {
 
-    for (i = 0; i < 2; i++) {
-        fs.readFile('datalist.txt', 'utf8', (err, data, __price) => {
-            __data = JSON.parse(data)
-            console.log(__data)
-        })
+    setTimeout(function() {
 
-        cryptonator.ticker('btc', 'usd')
-            .then(value => {
-                __price = value.ticker.price
-                console.log("im here")
-                return __price
-
-            }).then(value => {
-
-                fs.readFile('datalist.txt', 'utf8', (err, data, __price) => {
-                    if (err) {
-                        console.error(err)
-                        return
-                    }
-                    __data = JSON.parse(data)
-                    __price = value
-                    if (__price >= __data[0].price) {
-                        console.log(__data[0].price)
-                        console.log(__price)
-                        console.log("It is above")
-
-                        //Edits file here
-                        var result = data.replace(/54000/g, 'priceissued');
-
-                        fs.writeFile("datalist.txt", result, 'utf8', function(err) {
-                            if (err) return console.log(err);
-                        });
+        hello()
+        async function hello() {
 
 
-                    }
+            let __detect = db.get('posts')
+                .size()
+                .value()
 
-                })
+            for (i = 0; i < __detect; i++) {
 
 
-            })
-            .catch(console.error);
-    }
-}, 5000);
+
+                let __id = i
+                let john = db.get('posts')
+                    .find({ id: __id })
+                    .value()
+                __chatId = john.chatId
+                __ticker = john.ticker
+                __compare = john.compare
+                __com_price = john.com_price
+                __upndown = john.upndown
+                __status = john.status
+
+
+                //console.log(__chatId + '\n' + __ticker + '\n' + __compare + '\n' + __com_price + '\n' + __upndown + '\n' + __status)
+
+
+                await cryptonator.ticker(__ticker, __compare)
+                    .then(value => {
+
+                        __price = value.ticker.price
+                        if (__status == "active") {
+
+                            if (__upndown == "up" || __upndown == "UP" || __upndown == "Up" || __upndown == "uP") {
+                                if (__com_price <= __price) {
+                                    console.log(__price)
+                                    console.log(i)
+                                    console.log(__id)
+                                    console.log(__ticker)
+                                    console.log(__compare)
+                                    console.log(__com_price)
+                                    db.get('posts')
+                                        .find({ id: __id })
+                                        .assign({ status: "inactive" })
+                                        .write()
+                                    bot.sendMessage(__chatId, __ticker + '/' + __compare + " is above " + __com_price)
+
+                                }
+                            } else {
+                                if (__price <= __com_price) {
+
+
+                                    db.get('posts')
+                                        .find({ id: __id })
+                                        .assign({ status: "inactive" })
+                                        .write();
+                                    bot.sendMessage(__chatId, __ticker + '/' + __compare + " is down " + __com_price)
+
+                                }
+
+                            }
+                        }
+
+                        //console.log(__com_price)
+                    })
+                    .catch(console.error)
+            }
+        }
+    }, 100)
+
+}, 3600000);
